c49e527e57cbe8bb8801905acbe0359243fb82f9
Cas van Hees <146176177+CasvHees@users.noreply.github.com>
2024-04-07 13:39:39 +0200

    Add files via upload

---
+++ btcstats.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Perform API calls and store results
+logo='
+   ▄█▀▀▀▀▀█▄▄
+▄█▀░░▄░▄░░░░▀█▄
+█░░░▀█▀▀▀▀▄░░░█
+█░░░░█▄▄▄▄▀░░░█
+█░░░░█░░░░█░░░█
+▀█▄░▀▀█▀█▀░░▄█▀
+  ▀▀█▄▄▄▄▄█▀▀'
+btc_price=$(curl -sSL "https://mempool.space/api/v1/prices" | jq -r '.USD')
+block_height=$(curl -sSL "https://mempool.space/api/blocks/tip/height")
+block_tip_hash=$(curl -sSL "https://mempool.space/api/blocks/tip/hash")
+diff_adjustment=$(curl -sSL "https://mempool.space/api/v1/difficulty-adjustment" | jq -r '.difficultyChange')
+
+# Print BTC Information
+echo "$logo"
+echo "BTC PRICE: $btc_price"
+echo "BLOCK HEIGHT: $block_height"
+echo "BLOCK TIP HASH: $block_tip_hash"
+echo "DIFF ADJUSTMENT: $diff_adjustment"
